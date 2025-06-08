d2ed404e1d8c6b04b3fe9aa34811d1dae9e06fee
Davi Nogueira <davinogueira@ufmg.br>
2025-06-08 11:11:42 -0300

    Prototipo inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+Esse é um protótipo de software para a automatização das catracas dos Restaurantes Universitários da UFMG. 
+
+O projeto foi desenvolvido para o desafio da Engenharia de 2025, proposto pelo ENG200.
---
+++ script.js
@@ -0,0 +1,51 @@
+function quandoConfirmado(){
+    var valorRecarga = document.getElementById("numMeals");
+    var value = valorRecarga.value;
+    var text = valorRecarga.options[valorRecarga.selectedIndex].text;
+
+    window.location.href = `telaDePagamento.html?pay=${Math.round(value*5.6*100)/100}`
+
+    console.log("funcionou")
+}
+
+function atualizarValor(){
+    valores = document.querySelectorAll('strong')
+    
+    const urlParams = new URLSearchParams(window.location.search);
+    valor = urlParams.get('pay')
+    valor = parseFloat(valor).toFixed(2).toString()
+    valor = valor.replace('.',',')
+
+    i = 0
+    while(i < valores.length){
+        valores[i].innerText = `R$ ${valor}`
+        i++
+    }
+}
+
+function atualizarSaldo2(){
+    elemento = document.querySelector('#currentBalance')
+    saldo = localStorage.getItem('saldo_fump') | 0
+
+    saldo = saldo.toFixed(2).toString()
+    saldo = saldo.replace('.',',')
+    
+    elemento.innerText = `R$ ${saldo}`
+}
+
+function atualizarSaldo(){
+    const urlParams = new URLSearchParams(window.location.search);
+    recarga = urlParams.get('pay')
+    recarga = parseFloat(recarga)
+    
+    valor = localStorage.getItem('saldo_fump') | 0
+
+    novoSaldo = parseFloat(recarga + valor).toFixed(2)
+    localStorage.setItem("saldo_fump", novoSaldo);
+}
+
+function confirmarPagamento(){
+    atualizarSaldo()
+    window.location.href = `index.html`
+
+}
